60893c630c0940783e6e4a3085e6012104bbf0da
Thayanne Tenório <thayannenascimento7@gmail.com>
2024-02-24 16:08:47 -0300

    comment 2

--- src/main/java/com/project/professor/allocation/ProfessorAllocationThayruhliliApplication.java
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationThayruhliliApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfessorAllocationThayruhliliApplication {
-//futuro conflito by dev1
+//futuro conflito by dev1 aaaaaaa
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessorAllocationThayruhliliApplication.class, args);
 	}
