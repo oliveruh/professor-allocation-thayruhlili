a9e6b5b87f3ea08d2982ed2922bfc5f571da1e61
Ruhan Monte <herlisson.ruh@gmail.com>
2024-02-24 15:14:29 -0300

    add project

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationThayruhliliApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationThayruhliliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationThayruhliliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professor/allocation/ProfessorAllocationThayruhliliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationThayruhliliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
