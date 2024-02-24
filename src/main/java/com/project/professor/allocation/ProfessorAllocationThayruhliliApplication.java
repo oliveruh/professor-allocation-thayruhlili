1bf8696f0f52a91b6eb555ccc5ded079282d57c1
Ruhan Monte <herlisson.ruh@gmail.com>
2024-02-24 16:10:59 -0300

    commit aleatorio

--- src/main/java/com/project/professor/allocation/ProfessorAllocationThayruhliliApplication.java
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationThayruhliliApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfessorAllocationThayruhliliApplication {
-//futuro conflito by dev1
+// conflito conflitante - dev 3
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessorAllocationThayruhliliApplication.class, args);
 	}
