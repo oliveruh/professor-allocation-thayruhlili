b0edf908635a5e1d5bf3ad3dd9dda44ba2c2cfda
Thayanne Tenório <thayannenascimento7@gmail.com>
2024-02-24 16:06:36 -0300

    fogo no parquinho

--- src/main/java/com/project/professor/allocation/ProfessorAllocationThayruhliliApplication.java
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationThayruhliliApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfessorAllocationThayruhliliApplication {
-
+//futuro conflito by dev1
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessorAllocationThayruhliliApplication.class, args);
 	}
