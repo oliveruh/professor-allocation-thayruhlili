7bf6fe45159be95425f1b2c3bc74f2819944dff2
Thayanne Tenório <thayannenascimento7@gmail.com>
2024-04-20 12:41:01 -0300

    course controller

--- src/main/java/com/project/professor/allocation/controller/CourseController.java
+++ src/main/java/com/project/professor/allocation/controller/CourseController.java
@@ -1,10 +1,27 @@
 package com.project.professor.allocation.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.professor.allocation.entity.Course;
 import com.project.professor.allocation.service.CourseService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Courses")
@@ -18,5 +35,88 @@ public class CourseController {
         super();
         this.courseService = courseService;
     }
-    
+
+    @Operation(summary = "Pesquisa pelos cursos atraves do nome")
+    @ApiResponses({
+    	@ApiResponse(responseCode = "200", description = "Retorna cursos")
+    })
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Course>> findAll(@RequestParam(name = "name", required = false) String name) {
+        List<Course> courses = courseService.findAll(name);
+        return new ResponseEntity<>(courses, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Encontra um curso pelo id")
+    @ApiResponses({
+    	@ApiResponse(responseCode = "200", description = "Retorna um curso"),
+    	@ApiResponse(responseCode = "404", description = "Curso não encontrado", content = @Content),
+    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+    })
+    @GetMapping(path = "/{course_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Course> findById(@PathVariable(name = "course_id") Long id) {
+        Course course = courseService.findById(id);
+        if (course == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(course, HttpStatus.OK);
+        }
+    }
+
+    @Operation(summary = "Salva um curso")
+    @ApiResponses({
+    	@ApiResponse(responseCode = "201", description = "Retorna o novo curso adicionado."),
+    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+    })
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Course> save(@RequestBody Course course) {
+        try {
+        	Course courseSaved = courseService.save(course);
+            return new ResponseEntity<>(courseSaved, HttpStatus.CREATED);
+        } catch (Exception e) {
+        	return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Operation(summary = "Atualiza as informações de um curso a partir de um ID")
+    @ApiResponses({
+    	@ApiResponse(responseCode = "200", description = "Retorna o curso"),
+    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+    	@ApiResponse(responseCode = "404", description = "Curso nao encontrado", content = @Content)
+    })
+    @PutMapping(path = "/{course_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Course> update(@PathVariable(name = "course_id") Long id, @RequestBody Course course)
+  {
+        course.setId(id);
+        try {
+            course = courseService.update(course);
+            if (course == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            } else {
+                return new ResponseEntity<>(course, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+        	 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Operation(summary = "Deleta curso pelo ID.")
+    @ApiResponses({
+    	@ApiResponse(responseCode = "204", description = "Nenhum Conteúdo."),
+    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+    })
+    @DeleteMapping(path = "/{course_id}")
+    public ResponseEntity<Void> deleteById(@PathVariable(name = "course_id") Long id) {
+        courseService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @Operation(summary = "Deleta todos os cursos.")
+    @ApiResponses({
+    	@ApiResponse(responseCode = "204", description = "Nenhum Conteúdo.")
+    })
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAll() {
+        courseService.deleteAll();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
